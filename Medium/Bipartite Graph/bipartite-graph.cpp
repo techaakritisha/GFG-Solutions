09925cd6690c362dd1a398edbaf884d5661e2c74
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-12 12:09:07 -0100

    Added Solution - GfG to GitHub

---
+++ Medium/Bipartite Graph/bipartite-graph.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    bool solve(vector<int> adj[],int u, vector<int> &color, int curr) {
+        color[u] = curr;
+        
+        for(int &v : adj[u]){
+            if(color[v] == color[u]){
+                return false;
+            }
+            if(color[v] == -1){
+                int c = 1 - curr;
+             
+                if(solve(adj,v,color,c) == false){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+	bool isBipartite(int V, vector<int>adj[]){
+	   vector<int> color(V,-1);
+	   
+	   for(int i=0; i<V; i++) {
+	       if(color[i] == -1) {
+	           if(solve(adj,i,color,1) == false)
+	           return false;
+	       }
+	   }
+	   return true;
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int V, E;
+		cin >> V >> E;
+		vector<int>adj[V];
+		for(int i = 0; i < E; i++){
+			int u, v;
+			cin >> u >> v;
+			adj[u].push_back(v);
+			adj[v].push_back(u);
+		}
+		Solution obj;
+		bool ans = obj.isBipartite(V, adj);    
+		if(ans)cout << "1\n";
+		else cout << "0\n";  
+	}
+	return 0;
+}
+// } Driver Code Ends
