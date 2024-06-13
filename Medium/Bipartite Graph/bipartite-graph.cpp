c59665e332c93b16d95ac6f1b9d96bdd3eac1f5c
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-13 09:24:32 -0100

    Updated Solution - GfG to GitHub

--- Medium/Bipartite Graph/bipartite-graph.cpp
+++ Medium/Bipartite Graph/bipartite-graph.cpp
@@ -5,6 +5,7 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
+/*
     bool solve(vector<int> adj[],int u, vector<int> &color, int curr) {
         color[u] = curr;
         
@@ -32,7 +33,43 @@ public:
 	       }
 	   }
 	   return true;
-	}
+	   
+	   */
+	
+	
+     bool solveBFS(vector<int> adj[],int curr , vector<int> &color ,int currcolor) {
+         queue<int> q;
+         q.push(curr);
+         
+         color[curr] = currcolor;
+         
+         while(!q.empty()){
+             int u = q.front();
+             q.pop();
+             
+             for(int &v : adj[u]){
+                 if(color[v] == color[u]){
+                     return false;
+                 }
+                 if(color[v] == -1){
+                     color[v] = 1-color[u];
+                     q.push(v);
+                 }
+             }
+         }
+         return true;
+     }
+	bool isBipartite(int V, vector<int>adj[]){
+	  vector<int> color(V,-1);
+         
+         for(int i=0; i<V; i++){
+             if(color[i] == -1){
+                 if(solveBFS(adj,i,color,1) == false)
+                      return false;
+             }
+         }
+         return true;
+	} 
 };
 
 //{ Driver Code Starts.
