c9a2142dc4582fa7819dc6db6effbe1750e5e7a3
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-19 17:25:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find maximum volume of a cuboid/find-maximum-volume-of-a-cuboid.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    double maxVolume(double perimeter, double area) {
+       double peri = perimeter;
+       double ar = area;
+       
+       double len = (peri/2 - sqrt(peri * peri/4 - 6*ar))/6;
+       
+       double ans = len * len * (peri/4-2 * len);
+       
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    long long int t;
+    cin >> t;
+    while (t--) {
+        double perimeter, area;
+        cin >> perimeter >> area;
+        // if (area == 5 and parameter == 15) {
+        //     cout << "0.46" << endl;
+        //     return 0;
+        // }
+        Solution ob;
+        double ans = ob.maxVolume(perimeter, area);
+        cout.precision(2);
+        cout << fixed << ans << "\n";
+    }
+}
+// } Driver Code Ends
