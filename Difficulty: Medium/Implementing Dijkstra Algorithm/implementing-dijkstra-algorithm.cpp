cff3e5221d3384972ce6aa5adda55623a79c1658
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-06-23 17:02:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
+++ Difficulty: Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
@@ -7,7 +7,10 @@ class Solution
 {
 	public:
 	//Function to find the shortest distance of all the vertices
-    //from the source vertex S.
+    //from the source vertex S
+    
+    // USING MIN-HEAP
+    /*
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
@@ -37,6 +40,48 @@ class Solution
           }
         }
         return result;
+        
+        */
+        
+    // USING SET
+     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S) {
+            set<pair<int, int>> pq;
+
+            vector<int> result(V, INT_MAX);
+
+            result[S] = 0;
+            pq.insert({0, S});
+
+            while(!pq.empty()) {
+
+                auto &it = *pq.begin();
+                int d  = it.first;
+                int node = it.second;
+                pq.erase(it);
+
+                for(auto &vec : adj[node]) {
+
+                    int adjNode = vec[0];
+                    int dist    = vec[1];
+
+                    if(d + dist < result[adjNode]) {
+                        
+                        if(result[adjNode] != INT_MAX) {
+                            auto iter = pq.find({result[adjNode], adjNode});
+                            pq.erase(iter);
+			    //OR, pq.erase({result[adjNode], adjNode});
+                        }
+                        
+                        result[adjNode] = d + dist;
+                        pq.insert({d+dist, adjNode});
+
+                    }
+
+                }
+
+            }
+
+            return result;  
     }
 };
 
