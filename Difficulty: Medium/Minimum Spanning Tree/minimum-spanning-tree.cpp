f801826c0b4b4981974a487370f5728da47f1cee
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-07-02 10:36:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -7,6 +7,8 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+    /*
+    
     typedef pair<int,int> p;
     int spanningTree(int V, vector<vector<int>> adj[])
     {   
@@ -38,6 +40,79 @@ class Solution
            }
         }
         return sum;
+        
+        */
+    vector<int> rank;
+    vector<int> parent;
+    
+    void Union(int x , int y) {
+        int parent_x = find(x);
+        int parent_y = find(y);
+        
+        if(parent_x == parent_y)
+             return;
+             
+        if(rank[parent_x] > rank[parent_y]) {
+            parent[parent_y] = parent_x;
+        }
+        else if(rank[parent_y] > rank[parent_x]){
+            parent[parent_x] = parent_y;
+        }
+        else{
+            parent[parent_x] = parent_y; 
+            rank[parent_y]++;
+        }
+    }
+    int find(int x){
+        if(x == parent[x])
+         return x;
+         
+        return parent[x] = find(parent[x]);
+    }
+    int KruskalAlgorithm(vector<vector<int>> &vec) {
+        int sum = 0;
+        for(auto &it : vec){
+            int u = it[0];
+            int v = it[1];
+            int wt= it[2];
+            
+           int parent_u = find(u);
+           int parent_v = find(v);
+           
+           if(parent_u != parent_v){
+               Union(u,v);
+               sum += wt;
+           }
+        }
+        return sum;
+    }
+    int spanningTree(int V, vector<vector<int>> adj[]){
+          
+          vector<vector<int>> vec;
+          
+          parent.resize(V);
+          rank.resize(V,0);
+          
+          for(int i=0; i<V; i++){
+              parent[i] = i;
+          }
+          
+          for(int i=0; i<V; i++) {
+              for(auto &temp : adj[i]) {
+                  int u = temp[0];
+                  int wt = temp[1];
+                  
+                  vec.push_back({i,u,wt});
+              }
+          }
+          
+          auto comparator=[&](vector<int> &a , vector<int> &b) {
+              return a[2] < b[2];
+          };
+          
+          sort(begin(vec),end(vec),comparator);
+          
+          return KruskalAlgorithm(vec);
     }
 };
 
