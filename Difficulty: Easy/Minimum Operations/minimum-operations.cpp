1edf95561394a52b5d13ccbcfffe89a0b47b3044
Aakriti Shakya <132231250+techaakritisha@users.noreply.github.com>
2024-07-09 00:57:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Operations/minimum-operations.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    int minOperation(int n)
+    {
+        int cnt = 0;
+        
+        while(n > 0) {
+            if(n%2 == 0) {
+                n = n/2;
+                cnt++;
+            }
+            else{
+                n = n-1;
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	    {
+	        int n;
+	        cin>>n;
+	        Solution ob;
+	        cout<<ob.minOperation(n)<<endl;
+	    }
+}
+// } Driver Code Ends
